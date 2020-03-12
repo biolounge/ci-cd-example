17d33caaa88532938b6a034e0eb8045dcac97f36
Charly John <charly.john@philips.com>
2020-03-12 18:01:32 +0100

    new release

--- app.py
+++ app.py
@@ -1,4 +1,22 @@
 from flask import Flask
+from flask import render_template
+
+# creates a Flask application, named app
+app = Flask(__name__)
+
+# a route where we will display a welcome message via an HTML template
+@app.route("/")
+def hello():
+    message = "Hello, World"
+    return render_template('index.html', message=message)
+
+# run the application
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
+
+
+"""
+from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
@@ -12,3 +30,4 @@ def hello_name(name):
 
 if __name__ == '__main__':
     app.run()
+"""
